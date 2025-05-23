da6faf4c0ade2d5003b9a0650467ba1efdec7207
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-05-23 15:39:57 -0300

    fix: simplificado a estrutura de diretorios

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ provider "vultr" {
 }
 
 # Create a Vultr instance
-module "vultr_instance" {
-  source = "./modules/vultr_instance"
+resource "vultr_instance" "kubernetes-vultr-instance-demo" {
+    plan = var.vultr_instance_plan
+    region = var.vultr_instance_region
+    os_id = var.vultr_instance_os_id
+    count = var.vultr_instance_count
+    label = var.vultr_instance_name
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,43 @@
 # This file contains the variables used in the Terraform configuration for creating a Kubernetes cluster on Vultr.
+
+# This variable is used to specify the Vultr API key, which is stored in GitHub Secrets.
 variable "vultr_api_key" {
   description = "Vultr API Key stored in Gitub Secrets"
   type        = string
   sensitive   = true
 }
 
+# This variable is used to specify the name of the Vultr instance that will be created.
+variable "vultr_instance_name" {
+  description = "Name of the Vultr instance"
+  default = "kubernetes-vultr-instance-demo"
+  type    = string  
+}
+
+# This variable is used to specify the number of Vultr instances to create.
+variable "vultr_instance_count" {
+  description = "Number of Vultr instances to create"
+  default = 1
+  type    = number
+}
+
+# This variable is used to specify the plan, region, and OS ID for the Vultr instance.
+variable "vultr_instance_plan" {
+  description = "Vultr instance plan"
+  default = "vc2-1c-0.5gb-free"
+  type    = string
+}
+
+# This variable is used to specify the region and OS ID for the Vultr instance.
+variable "vultr_instance_region" {
+  description = "Vultr instance region"
+  default = "mia"
+  type    = string
+}
+
+# This variable is used to specify the OS ID for the Vultr instance.
+variable "vultr_instance_os_id" {
+  description = "Vultr instance OS ID"
+  default = 387
+  type    = number
+}
