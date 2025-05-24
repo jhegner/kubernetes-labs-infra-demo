f2a0a738b977d3b220adc1418d92bfe300d60975
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 18:57:44 -0300

    test

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket       = "bucket-kubernetes-labs-terraform-state"
-    key          = "terraform.tfstate"
-    region       = "sa-east-1"
+    bucket         = "bucket-kubernetes-labs-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "sa-east-1"
     dynamodb_table = "terraform-lock"
-    encrypt      = true
+    encrypt        = true
   }
 }
