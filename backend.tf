2b51051c2b1c622251babd3be6ae981211d9c008
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 18:53:23 -0300

    fix: teste alteracao de regiao aws

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "bucket-kubernetes-labs-terraform-state"
     key          = "terraform.tfstate"
-    region       = "us-east-1"
+    region       = "sa-east-1"
     use_lockfile = true
     encrypt      = true
   }
