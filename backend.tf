6a4f773cfd1d2d6d22428b94bb238d8326aaa72e
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 19:02:28 -0300

    chore: voltando para use_lockfile

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "bucket-kubernetes-labs-terraform-state"
     key            = "infra/terraform.tfstate"
     region         = "sa-east-1"
-    dynamodb_table = "terraform-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
