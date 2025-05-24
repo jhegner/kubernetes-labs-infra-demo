41c4369e7342560bfaf83c7093af0f47ea972cdf
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 17:33:27 -0300

    feat: salva o tfstate no bucket s3

--- README.md
+++
@@ -1,7 +0,0 @@
-# Kubernetes Labs - Infra Demo
-
-## Repositório para testes e demo, para:
-
-- Github Actions (workflow, jobs, etc)
-- Terraform
-- Vultr
--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
 terraform {
-  required_providers {
-    vultr = {
-      source = "vultr/vultr"
-      version = "2.26.0"
-    }
+  backend "s3" {
+    bucket = "bucket-kubernetes-labs-terraform-state"
+    key = "infra/terraform.tfstate" 
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt = true
   }
-}
-
-# Configure the Vultr Provider
-provider "vultr" {
-  api_key = var.VULTR_API_KEY
-}
-
-# Create a Vultr instance
-resource "vultr_instance" "kubernetes-vultr-instance-demo" {
-    plan = var.vultr_instance_plan
-    region = var.vultr_instance_region
-    os_id = var.vultr_instance_os_id
-    count = var.vultr_instance_count
-    label = var.vultr_instance_name
-    backups = var.vultr_instance_backups
 }
