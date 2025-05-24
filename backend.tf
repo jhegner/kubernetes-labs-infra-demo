04ce3ad45c473c22a4a175589a5bb2cde1c62da2
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 18:57:36 -0300

    fix: teste dynamodb_table

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "bucket-kubernetes-labs-terraform-state"
     key          = "terraform.tfstate"
     region       = "sa-east-1"
-    use_lockfile = true
+    dynamodb_table = "terraform-lock"
     encrypt      = true
   }
 }
