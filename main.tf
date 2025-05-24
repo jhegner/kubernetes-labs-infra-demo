e7833966ffdfd1f22cadda713630008e7b8c05cd
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 17:42:35 -0300

    fix: error Inappropriate value for attribute "use_lockfile"

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "bucket-kubernetes-labs-terraform-state"
     key = "infra/terraform.tfstate" 
     region = "us-east-1"
-    use_lockfile = "terraform-lock"
+    use_lockfile = true
     encrypt = true
   }
 }
