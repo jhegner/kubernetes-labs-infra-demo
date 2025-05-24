cf856ed9f43855fa6fe033ed8239aba478069f9e
Guilherme Fernandez <guil.fernandez1@hotmail.com>
2025-05-24 17:53:44 -0300

    fix: ajuste key teste

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "bucket-kubernetes-labs-terraform-state"
-    key = "terraform.tfstate" 
+    key = "env:/terraform.tfstate" 
     region = "us-east-1"
     use_lockfile = true
     encrypt = true
