7badbef6826782d1ad50d50a88e287b6b65ce75d
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-05-30 19:13:48 -0300

    chore: demo usando terraform s3 backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "bucket-kubernetes-labs-terraform-backend"
-    key            = "bucket-kubernetes-labs-terraform-backend/lab/state/key.tflock"
+    bucket         = "kubernetes-labs-tf-backend-aws-s3bucket"
+    key            = "lab/state/terraform.tflock"
     region         = "us-east-1"
     use_lockfile   = true
   }
