ab96e65b1d2a8a3849f14b36e538664adc976989
Jidlafe Hegner <jidlafehegner@gmail.com>
2025-05-30 20:15:10 -0300

    chore: executando action

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "kubernetes-labs-tf-backend-aws-s3bucket"
-    key            = "lab/state/terraform"
+    key            = "lab/state/terraform.tflock"
     region         = "us-east-1"
     use_lockfile   = true
   }
